ae8b811372b88a2d5ab72859bff27e53d5d14023
SindhuR7 <alirazasindhu066@gmail.com>
2025-09-21 23:06:17 +0500

    Navbar

--- Pato Place/src/components/Navbar.jsx
+++ Pato Place/src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import logo from '../assets/logo.png.webp'
-import '../components/Navbar.css'
+
 
 const Navbar = () => {
   return (
     <>
-    <nav>
-        <div className="left">
-            <img src={logo} alt="Pato" />
+    <nav className='flex justify-between items-center bg-amber-400 py-3.5 px-16'>
+        <div >
+            <img className=" transition-none" src={logo} alt="Pato" />
         </div>
         <div className="center">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Menu</a></li>
-                <li><a href="#">Reservation</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact</a></li>
+            <ul className='flex items-center list-style-type: none gap-14  text-white text-lg font-serif'>
+                <li className='hover:text-red-600 transition-colors'><a href="#">Home</a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#">Menu</a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#">Reservation</a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#">Gallery</a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#">About</a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#">Blog</a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#">Contact</a></li>
             </ul>
         </div>
-        <div className="right">
-            <ul>
-                <li><a href="#"><i class="fa-solid fa-alarm-clock"></i></a></li>
-                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                <li><a href='#'><i class="fa-solid fa-bars"></i></a></li>
+        <div >
+            <ul className="flex items-center list-style-type: none gap-7 text-white text-base">
+                <li className='hover:text-red-600 transition-colors'><a href="#"><i class="fa-solid fa-alarm-clock"></i></a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                <li className='hover:text-red-600 transition-colors'><a href='#'><i class="fa-solid fa-bars"></i></a></li>
             </ul>
         </div>
     </nav>
