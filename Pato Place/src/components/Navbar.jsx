426371ca80ff7a68cbaca96d4d74b0fd743c9ee7
SindhuR7 <alirazasindhu066@gmail.com>
2025-09-22 21:10:17 +0500

    Navbar

--- Pato Place/src/components/Navbar.jsx
+++ Pato Place/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import logo from '../assets/logo.png.webp'
+import { FaFacebookF } from "react-icons/fa";
+import { TiSocialTwitter } from "react-icons/ti";
+import { CiMenuFries } from "react-icons/ci";
+import { LiaTripadvisor } from "react-icons/lia";
 
 
 const Navbar = () => {
   return (
     <>
-    <nav className='flex justify-between items-center bg-amber-400 py-3.5 px-16'>
+    <nav className='flex justify-between items-center bg-transparent  py-6 px-6  md:px-16'>
         <div >
-            <img className=" transition-none" src={logo} alt="Pato" />
+            <img className=" transition-none cursor-pointer h-11 md:h-full" src={logo} alt="Pato" />
         </div>
-        <div className="center">
-            <ul className='flex items-center list-style-type: none gap-14  text-white text-lg font-serif'>
+        <div className=''>
+            <ul className='hidden md:flex items-center list-style-type: none gap-14  text-white text-lg font-serif '>
                 <li className='hover:text-red-600 transition-colors'><a href="#">Home</a></li>
                 <li className='hover:text-red-600 transition-colors'><a href="#">Menu</a></li>
                 <li className='hover:text-red-600 transition-colors'><a href="#">Reservation</a></li>
@@ -21,11 +25,11 @@ const Navbar = () => {
             </ul>
         </div>
         <div >
-            <ul className="flex items-center list-style-type: none gap-7 text-white text-base">
-                <li className='hover:text-red-600 transition-colors'><a href="#"><i class="fa-solid fa-alarm-clock"></i></a></li>
-                <li className='hover:text-red-600 transition-colors'><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li className='hover:text-red-600 transition-colors'><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                <li className='hover:text-red-600 transition-colors'><a href='#'><i class="fa-solid fa-bars"></i></a></li>
+            <ul className="flex items-center list-style-type: none gap-3 text-white text-lg md:gap-7 md:text-2xl">
+                <li className='hover:text-red-600 transition-colors'><a href="#"><LiaTripadvisor /></a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#"><FaFacebookF /></a></li>
+                <li className='hover:text-red-600 transition-colors'><a href="#"><TiSocialTwitter /></a></li>
+                <li className='hover:text-red-600 transition-colors'><a href='#'><CiMenuFries /></a></li>
             </ul>
         </div>
     </nav>
