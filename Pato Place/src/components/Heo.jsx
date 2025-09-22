49d1ab35dfe96958a9d162b8371e0f7039c35d66
SindhuR7 <alirazasindhu066@gmail.com>
2025-09-22 23:23:00 +0500

    Hero Section

--- Pato Place/src/components/Heo.jsx
+++ Pato Place/src/components/Heo.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import backgroundImage from '../assets/master-slides-02.jpg'
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+
 const Heo = () => {
   return (
     <>
-    <section className='h-lvh absolute bg-[url("../assets/master-slides-02.jpg")] '>
-        <div className="title">Welcome to</div>
-        <div className="heading">Pato Place</div>
+    <section className=' bg-[url("./assets/master-slides-02.jpg")] h-lvh w-full bg-cover bg-center bg-no-repeat absolute top-0 left-0 -z-10 group'>
+        <div className="flex flex-col items-center justify-center text-white h-full w-full z-10">
+          <div className="font-['Courgette',cursive] text-6xl italic md:text-7xl">Welcome to</div>
+        <div className="font-['Poppins', sans-serif] text-7xl text-center font-bold tracking-[0.75rem] uppercase mt-5 md:text-8xl">Pato Place</div>
         <div className="btn">
-            <button>Look Menu</button>
+            <button className='bg-white text-[#ec1d25] text-lg px-7 py-2 mt-6 rounded-2xl cursor-pointer hover:bg-[#ec1d25] hover:text-white transition-colors duration-400 animate-pulse hover:animate-none' >Look Menu</button>
         </div>
-        <div className="points">
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
         </div>
-        <div className="arrow"><i class="fa-solid fa-arrow-left"></i></div>
+        <div className="flex justify-center items-center gap-2.5 -mt-10">
+            <div className="h-3 w-3 bg-gray-400 rounded-lg cursor-pointer hover:bg-red-600 hover:border-amber-100 hover:border-2 transition-all duration-300  border-1 border-transparent"></div>
+            <div className="h-3 w-3 bg-gray-400 rounded-lg cursor-pointer hover:bg-red-600 hover:border-amber-100 hover:border-2 transition-all duration-300  border-1 border-transparent"></div>
+            <div className="h-3 w-3 bg-gray-400 rounded-lg cursor-pointer hover:bg-red-600 hover:border-amber-100 hover:border-2 transition-all duration-300  border-1 border-transparent"></div>
+        </div>
+        <div className='h-14 w-14 rounded-4xl bg-[#0000004b] flex items-center justify-center text-white text-xl absolute top-[50%] left-[5%] opacity-0 group-hover:opacity-100 hover:bg-[#ec1a25] transition-opacity duration-500 cursor-pointer' ><MdOutlineKeyboardArrowLeft /></div>
+        <div className='h-14 w-14 rounded-4xl bg-[#0000004b] flex items-center justify-center text-white text-xl absolute top-[50%] right-[5%] opacity-0 group-hover:opacity-100 hover:bg-[#ec1a25] transition-opacity duration-500 cursor-pointer' ><MdKeyboardArrowRight /></div>
     </section>
     </>
   )
