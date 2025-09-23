68704a9147c614c2af8396f3ee4d779424850f9f
SindhuR7 <alirazasindhu066@gmail.com>
2025-09-23 21:45:04 +0500

    Story Section

--- Pato Place/src/components/Second.jsx
+++ Pato Place/src/components/Second.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import '../components/Second.css'
 import image from '../assets/our-story-01.jpg.webp'
+import { MdOutlineArrowRightAlt } from "react-icons/md";
+
 
 const Second = () => {
   return (
     <>
-    <section className='ourStory'>
-        <div className="leftDiv">
-            <div className="main title">Italian Resturant</div>
-            <div className="wel heading">WELCOME</div>
-            <div className="des">Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est. Sed fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi, ac facilisis ligula sem id neque.</div>
-            <div className="go">Our Story <i class="fa-solid fa-arrow-right"></i></div>
+    <section className='flex flex-col justify-center items-center mt-125 bg-[#2125292f] py-24 gap-15 md:flex-row md:gap-25'>
+        <div className="flex flex-col items-center justify-center">
+            <div className='font-["Courgette",cursive] italic text-2xl text-[#ec1a25] md:text-3xl'>Italian Resturant</div>
+            <div className="font-['Poppins', sans-serif] text-6xl text-center font-bold tracking-widest mt-2 mb-10">WELCOME</div>
+            <div className="font-['Montserrat',sans-serif] text-[#666666] text-lg w-sm text-center md:w-lg">Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est. Sed fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi, ac facilisis ligula sem id neque.</div>
+            <div className="flex justify-center items-center font-['Montserrat',sans-serif] text-[#666666] cursor-pointer text-center text-lg mt-8 hover:text-[#ec1a25] transition-colors duration-500 md:mt-14">Our Story <MdOutlineArrowRightAlt /></div>
         </div>
-        <div className="rightDiv">
-            <img src={image} alt="Story" />
+        <div className="w-sm overflow-hidden rounded-3xl">
+            <img className='w-full hover:scale-120 cursor-pointer transition-transform duration-500' src={image} alt="Story" />
         </div>
     </section>
     </>
