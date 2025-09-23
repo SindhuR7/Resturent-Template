ddb21185342e73dcfa83f380aaa32e1c78bd7e94
SindhuR7 <alirazasindhu066@gmail.com>
2025-09-23 21:45:21 +0500

    Discover

--- Pato Place/src/components/Third.jsx
+++ Pato Place/src/components/Third.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import '../components/Third.css'
 
 const Third = () => {
   return (
     <>
-    <div className="image">
-        <div className="title dis">Discover</div>
-        <div className="heading ti">PATO PLACE</div>
+    <div className='bg-[url("./assets/bg-intro-01.jpg")] bg-center bg-cover bg-no-repeat h-80 w-full bg-fixed flex flex-col justify-center items-center'>
+    <div className='font-["Courgette",cursive] italic text-4xl text-[#ec1a25]'>Dicover</div>
+    <div className='font-["Poppins", sans-serif] text-6xl text-center font-bold tracking-widest mt-2 mb-10 text-white'>PATO PLACE</div>
     </div>
     </>
   )
